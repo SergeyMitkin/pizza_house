e522140abfbc663c1b6ff53b8797ea746e701a8b
SergeyMitkin <sergeymitkin@gmail.com>
2022-09-18 22:42:26 +0300

    Initial commit

---
+++ resources/views/pizzas/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.layout')
+
+@section('content')
+
+    <div class="wrapper pizza-details">
+        <h1>Order for {{ $pizza->name }}</h1>
+        <p class="type">Type - {{ $pizza->type }}</p>
+        <p class="base">Base - {{ $pizza->base }}</p>
+        <p class="toppings">Extra toppings</p>
+        <ul>
+            @foreach($pizza->toppings as $topping)
+                <li>{{ $topping }}</li>
+            @endforeach
+        </ul>
+
+        <form action="/pizzas/{{ $pizza->id }}" method="post">
+            {{ csrf_field() }}
+            {{ method_field('delete') }}
+            <button>Complete Order</button>
+        </form>
+    </div>
+    <a href="/pizzas" class="back"><- Back to all pizzas</a>
+
+@endsection
+
+
